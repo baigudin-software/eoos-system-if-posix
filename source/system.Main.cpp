e3a48d64b22ca51aa266c00e44e8b719a59a09df
Sergey Baigudin <sbaigudin@luxoft.com>
2018-09-10 18:03:12 +0300

    Basic source files added

---
+++ source/system.Main.cpp
@@ -0,0 +1,18 @@
+/** 
+ * The main entery point.
+ * 
+ * @author    Sergey Baigudin, sergey@baigudin.software
+ * @copyright 2018, Embedded Team, Sergey Baigudin
+ * @license   http://embedded.team/license/
+ */
+#include "system.System.hpp"
+
+/**
+ * Executes a user application main process.
+ *
+ * @param args - an application arguments.
+ */
+int main(void* const args)
+{
+    return 0;
+}
